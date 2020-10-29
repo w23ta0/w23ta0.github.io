95db040edfa1cfb60a438fbe57c849196d2ba91a
w23ta0 <wt8566958@gmail.com>
2020-10-29 22:06:44 +0800

    Site updated: 2020-10-29 22:06:43

--- love/js/functions_dev.js
+++ love/js/functions_dev.js
@@ -1,1 +1,150 @@
-var gardenCtx,gardenCanvas,$garden,garden,$window=$(window),clientWidth=$(window).width(),clientHeight=$(window).height();function getHeartPoint(t){var e=t/Math.PI,n=16*Math.pow(Math.sin(e),3)*19.5,s=-20*(13*Math.cos(e)-5*Math.cos(2*e)-2*Math.cos(3*e)-Math.cos(4*e));return new Array(offsetX+n,offsetY+s)}function startHeartAnimation(){var a=10,o=new Array,i=setInterval(function(){for(var t=getHeartPoint(a),e=!0,n=0;n<o.length;n++){var s=o[n];if(Math.sqrt(Math.pow(s[0]-t[0],2)+Math.pow(s[1]-t[1],2))<1.3*Garden.options.bloomRadius.max){e=!1;break}}e&&o.push(t),30<=a?(clearInterval(i),showMessages()):a+=.2},50)}function getDaysInMonth(t){return[31,28,31,30,31,30,31,31,30,31,30,31][t]}function timeElapse(t,e){var n=new Date,s=NaN,a=NaN,o=NaN,i=NaN,r=NaN,h=NaN;(h=n.getSeconds()-t.getSeconds())<0&&(h+=60,n.setMinutes(n.getMinutes()-1)),(r=n.getMinutes()-t.getMinutes())<0&&(r+=60,n.setHours(n.getHours()-1)),(i=n.getHours()-t.getHours())<0&&(i+=24,n.setDate(n.getDate()-1)),1==e?((o=n.getDate()-t.getDate())<0&&(o+=getDaysInMonth(n.getMonth()),n.setDate(n.getDate()-1)),(a=n.getMonth()-t.getMonth())<0&&(a+=12,n.setYear(n.getFullYear()-1)),s=n.getFullYear()-t.getFullYear()):o=Math.floor((n.getTime()-t.getTime())/864e5),i<10&&(i="0"+i),r<10&&(r="0"+r),h<10&&(h="0"+h);var g=0<s?'<span class="digit"><h3>'+s+"</h3> years </span>":"";g+=0<=a?'<span class="digit"><h3>'+a+"</h3> months </span>":"",g+='<span class="digit"><h3>'+o+"</h3> days </span>",g+='<span class="digit"><h3>'+i+"</h3> hours </span>",g+='<span class="digit"><h3>'+r+"</h3> minutes </span>",g+='<span class="digit"><h3>'+h+"</h3> seconds</span>",$("#elapseClock").html(g)}function showMessages(){adjustWordsPosition(),$("#messages").fadeIn(5e3,function(){showLoveU()})}function adjustWordsPosition(){$("#words").css("position","absolute")}function adjustCodePosition(){$("#code").css("margin-top",($("#garden").height()-$("#code").height())/2)}function showLoveU(){$("#loveu").fadeIn(3e3)}$(window).resize(function(){var t=$(window).width(),e=$(window).height();t!=clientWidth&&e!=clientHeight&&location.replace(location)}),function(a){a.fn.typewriter=function(){return this.each(function(){var t=a(this),e=t.html(),n=0;t.html("");var s=setInterval(function(){"<"==e.substr(n,1)?n=e.indexOf(">",n)+1:n++,t.html(e.substring(0,n)+(1&n?"_":"")),n>=e.length&&clearInterval(s)},75)}),this}}(jQuery);
+// variables
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(window).resize(function() {
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+function getHeartPoint(angle) {
+	var t = angle / Math.PI;
+	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
+	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	return new Array(offsetX + x, offsetY + y);
+}
+
+function startHeartAnimation() {
+	var interval = 50;
+	var angle = 10;
+	var heart = new Array();
+	var animationTimer = setInterval(function () {
+		var bloom = getHeartPoint(angle);
+		var draw = true;
+		for (var i = 0; i < heart.length; i++) {
+			var p = heart[i];
+			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+			if (distance < Garden.options.bloomRadius.max * 1.3) {
+				draw = false;
+				break;
+			}
+		}
+		if (draw) {
+			heart.push(bloom);
+		}
+		if (angle >= 30) {
+			clearInterval(animationTimer);
+			showMessages();
+		} else {
+			angle += 0.2;
+		}
+	}, interval);
+}
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function getDaysInMonth(month) {
+	var data = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	return data[month];
+}
+
+function timeElapse(date, mode) {
+	var current = new Date();
+	var years = NaN;
+	var months = NaN;
+	var days = NaN;
+	var hours = NaN;
+	var minutes = NaN;
+	var seconds = NaN;
+	seconds = current.getSeconds() - date.getSeconds();
+	if (seconds < 0) {
+		seconds += 60;
+		current.setMinutes(current.getMinutes() - 1);
+	}
+	minutes = current.getMinutes() - date.getMinutes();
+	if (minutes < 0) {
+		minutes += 60;
+		current.setHours(current.getHours() - 1);
+	}
+	hours = current.getHours() - date.getHours();
+	if (hours < 0) {
+		hours += 24;
+		current.setDate(current.getDate() - 1);
+	}
+	if (mode == 1) {
+		days = current.getDate() - date.getDate();
+		if (days < 0) {
+			days += getDaysInMonth(current.getMonth());
+			current.setDate(current.getDate() - 1);
+		}
+		months = current.getMonth() - date.getMonth();
+		if (months < 0) {
+			months += 12;
+			current.setYear(current.getFullYear() - 1);
+		}
+		years = current.getFullYear() - date.getFullYear();
+	} else {
+		days = Math.floor((current.getTime() - date.getTime()) / (1000 * 3600 * 24));
+	}
+
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = (years > 0 ? "<span class=\"digit\"><h3>" + years + "</h3> years </span>":"")
+	result += (months >= 0 ? "<span class=\"digit\"><h3>" + months + "</h3> months </span>":"");
+	result += "<span class=\"digit\"><h3>"+ days + "</h3> days </span>";
+	result += "<span class=\"digit\"><h3>" + hours + "</h3> hours </span>"
+	result += "<span class=\"digit\"><h3>" + minutes + "</h3> minutes </span>"
+	result += "<span class=\"digit\"><h3>" + seconds + "</h3> seconds</span>";
+	
+	$("#elapseClock").html(result);
+}
+
+function showMessages() {
+	adjustWordsPosition();
+	$('#messages').fadeIn(5000, function() {
+		showLoveU();
+	});
+}
+
+function adjustWordsPosition() {
+	$('#words').css("position", "absolute");
+}
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+}
+
+function showLoveU() {
+	$('#loveu').fadeIn(3000);
+}
--- love/js/garden_dev.js
+++ love/js/garden_dev.js
@@ -1,1 +1,199 @@
-function Vector(t,n){this.x=t,this.y=n}function Petal(t,n,o,r,e,i){this.stretchA=t,this.stretchB=n,this.startAngle=o,this.angle=r,this.bloom=i,this.growFactor=e,this.r=1,this.isfinished=!1}function Bloom(t,n,o,r,e){this.p=t,this.r=n,this.c=o,this.pc=r,this.petals=[],this.garden=e,this.init(),this.garden.addBloom(this)}function Garden(t,n){this.blooms=[],this.element=n,this.ctx=t}Vector.prototype={rotate:function(t){var n=this.x,o=this.y;return this.x=Math.cos(t)*n-Math.sin(t)*o,this.y=Math.sin(t)*n+Math.cos(t)*o,this},mult:function(t){return this.x*=t,this.y*=t,this},clone:function(){return new Vector(this.x,this.y)},length:function(){return Math.sqrt(this.x*this.x+this.y*this.y)},subtract:function(t){return this.x-=t.x,this.y-=t.y,this},set:function(t,n){return this.x=t,this.y=n,this}},Petal.prototype={draw:function(){var t,n,o,r,e=this.bloom.garden.ctx;n=(t=new Vector(0,this.r).rotate(Garden.degrad(this.startAngle))).clone().rotate(Garden.degrad(this.angle)),o=t.clone().mult(this.stretchA),r=n.clone().mult(this.stretchB),e.strokeStyle=this.bloom.c,e.beginPath(),e.moveTo(t.x,t.y),e.bezierCurveTo(o.x,o.y,r.x,r.y,n.x,n.y),e.stroke()},render:function(){this.r<=this.bloom.r?(this.r+=this.growFactor,this.draw()):this.isfinished=!0}},Bloom.prototype={draw:function(){var t,n=!0;this.garden.ctx.save(),this.garden.ctx.translate(this.p.x,this.p.y);for(var o=0;o<this.petals.length;o++)(t=this.petals[o]).render(),n*=t.isfinished;this.garden.ctx.restore(),1==n&&this.garden.removeBloom(this)},init:function(){for(var t=360/this.pc,n=Garden.randomInt(0,90),o=0;o<this.pc;o++)this.petals.push(new Petal(Garden.random(Garden.options.petalStretch.min,Garden.options.petalStretch.max),Garden.random(Garden.options.petalStretch.min,Garden.options.petalStretch.max),n+o*t,t,Garden.random(Garden.options.growFactor.min,Garden.options.growFactor.max),this))}},Garden.prototype={render:function(){for(var t=0;t<this.blooms.length;t++)this.blooms[t].draw()},addBloom:function(t){this.blooms.push(t)},removeBloom:function(t){for(var n=0;n<this.blooms.length;n++)if(this.blooms[n]===t)return this.blooms.splice(n,1),this},createRandomBloom:function(t,n){this.createBloom(t,n,Garden.randomInt(Garden.options.bloomRadius.min,Garden.options.bloomRadius.max),Garden.randomrgba(Garden.options.color.rmin,Garden.options.color.rmax,Garden.options.color.gmin,Garden.options.color.gmax,Garden.options.color.bmin,Garden.options.color.bmax,Garden.options.color.opacity),Garden.randomInt(Garden.options.petalCount.min,Garden.options.petalCount.max))},createBloom:function(t,n,o,r,e){new Bloom(new Vector(t,n),o,r,e,this)},clear:function(){this.blooms=[],this.ctx.clearRect(0,0,this.element.width,this.element.height)}},Garden.options={petalCount:{min:8,max:15},petalStretch:{min:.1,max:3},growFactor:{min:.1,max:1},bloomRadius:{min:8,max:10},density:10,growSpeed:1e3/60,color:{rmin:128,rmax:255,gmin:0,gmax:128,bmin:0,bmax:128,opacity:.1},tanAngle:60},Garden.random=function(t,n){return Math.random()*(n-t)+t},Garden.randomInt=function(t,n){return Math.floor(Math.random()*(n-t+1))+t},Garden.circle=2*Math.PI,Garden.degrad=function(t){return Garden.circle/360*t},Garden.raddeg=function(t){return t/Garden.circle*360},Garden.rgba=function(t,n,o,r){return"rgba("+t+","+n+","+o+","+r+")"},Garden.randomrgba=function(t,n,o,r,e,i,a){var s=Math.round(Garden.random(t,n)),h=Math.round(Garden.random(o,r)),d=Math.round(Garden.random(e,i));return Math.abs(s-h)<=5&&Math.abs(h-d)<=5&&Math.abs(d-s)<=5?Garden.rgba(t,n,o,r,e,i,a):Garden.rgba(s,h,d,a)};
+	    function Vector(x, y) {
+	        this.x = x;
+	        this.y = y;
+	    };
+		
+	    Vector.prototype = {
+	        rotate: function (theta) {
+	            var x = this.x;
+	            var y = this.y;
+	            this.x = Math.cos(theta) * x - Math.sin(theta) * y;
+	            this.y = Math.sin(theta) * x + Math.cos(theta) * y;
+	            return this;
+	        },
+	        mult: function (f) {
+	            this.x *= f;
+	            this.y *= f;
+	            return this;
+	        },
+	        clone: function () {
+	            return new Vector(this.x, this.y);
+	        },
+	        length: function () {
+	            return Math.sqrt(this.x * this.x + this.y * this.y);
+	        },
+	        subtract: function (v) {
+	            this.x -= v.x;
+	            this.y -= v.y;
+	            return this;
+	        },
+	        set: function (x, y) {
+	            this.x = x;
+	            this.y = y;
+	            return this;
+	        }
+	    };
+		
+	    function Petal(stretchA, stretchB, startAngle, angle, growFactor, bloom) {
+	        this.stretchA = stretchA;
+	        this.stretchB = stretchB;
+	        this.startAngle = startAngle;
+	        this.angle = angle;
+	        this.bloom = bloom;
+	        this.growFactor = growFactor;
+	        this.r = 1;
+	        this.isfinished = false;
+	        //this.tanAngleA = Garden.random(-Garden.degrad(Garden.options.tanAngle), Garden.degrad(Garden.options.tanAngle));
+	        //this.tanAngleB = Garden.random(-Garden.degrad(Garden.options.tanAngle), Garden.degrad(Garden.options.tanAngle));
+	    }
+	    Petal.prototype = {
+	        draw: function () {
+	            var ctx = this.bloom.garden.ctx;
+	            var v1, v2, v3, v4;
+	            v1 = new Vector(0, this.r).rotate(Garden.degrad(this.startAngle));
+	            v2 = v1.clone().rotate(Garden.degrad(this.angle));
+	            v3 = v1.clone().mult(this.stretchA); //.rotate(this.tanAngleA);
+	            v4 = v2.clone().mult(this.stretchB); //.rotate(this.tanAngleB);
+	            ctx.strokeStyle = this.bloom.c;
+	            ctx.beginPath();
+	            ctx.moveTo(v1.x, v1.y);
+	            ctx.bezierCurveTo(v3.x, v3.y, v4.x, v4.y, v2.x, v2.y);
+	            ctx.stroke();
+	        },
+	        render: function () {
+	            if (this.r <= this.bloom.r) {
+	                this.r += this.growFactor; // / 10;
+	                this.draw();
+	            } else {
+	                this.isfinished = true;
+	            }
+	        }
+	    }
+
+	    function Bloom(p, r, c, pc, garden) {
+	        this.p = p;
+	        this.r = r;
+	        this.c = c;
+	        this.pc = pc;
+	        this.petals = [];
+	        this.garden = garden;
+	        this.init();
+	        this.garden.addBloom(this);
+	    }
+	    Bloom.prototype = {
+	        draw: function () {
+	            var p, isfinished = true;
+	            this.garden.ctx.save();
+	            this.garden.ctx.translate(this.p.x, this.p.y);
+	            for (var i = 0; i < this.petals.length; i++) {
+	                p = this.petals[i];
+	                p.render();
+	                isfinished *= p.isfinished;
+	            }
+	            this.garden.ctx.restore();
+	            if (isfinished == true) {
+	                this.garden.removeBloom(this);
+	            }
+	        },
+	        init: function () {
+	            var angle = 360 / this.pc;
+	            var startAngle = Garden.randomInt(0, 90);
+	            for (var i = 0; i < this.pc; i++) {
+	                this.petals.push(new Petal(Garden.random(Garden.options.petalStretch.min, Garden.options.petalStretch.max), Garden.random(Garden.options.petalStretch.min, Garden.options.petalStretch.max), startAngle + i * angle, angle, Garden.random(Garden.options.growFactor.min, Garden.options.growFactor.max), this));
+	            }
+	        }
+	    }
+
+	    function Garden(ctx, element) {
+	        this.blooms = [];
+	        this.element = element;
+	        this.ctx = ctx;
+	    }
+	    Garden.prototype = {
+	        render: function () {
+	            for (var i = 0; i < this.blooms.length; i++) {
+	                this.blooms[i].draw();
+	            }
+	        },
+	        addBloom: function (b) {
+	            this.blooms.push(b);
+	        },
+	        removeBloom: function (b) {
+	            var bloom;
+	            for (var i = 0; i < this.blooms.length; i++) {
+	                bloom = this.blooms[i];
+	                if (bloom === b) {
+	                    this.blooms.splice(i, 1);
+	                    return this;
+	                }
+	            }
+	        },
+	        createRandomBloom: function (x, y) {
+	            this.createBloom(x, y, Garden.randomInt(Garden.options.bloomRadius.min, Garden.options.bloomRadius.max), Garden.randomrgba(Garden.options.color.rmin, Garden.options.color.rmax, Garden.options.color.gmin, Garden.options.color.gmax, Garden.options.color.bmin, Garden.options.color.bmax, Garden.options.color.opacity), Garden.randomInt(Garden.options.petalCount.min, Garden.options.petalCount.max));
+	        },
+	        createBloom: function (x, y, r, c, pc) {
+	            new Bloom(new Vector(x, y), r, c, pc, this);
+	        },
+	        clear: function () {
+	            this.blooms = [];
+	            this.ctx.clearRect(0, 0, this.element.width, this.element.height);
+	        }
+	    }
+
+	    Garden.options = {
+	        petalCount: {
+	            min: 8,
+	            max: 15
+	        },
+	        petalStretch: {
+	            min: 0.1,
+	            max: 3
+	        },
+	        growFactor: {
+	            min: 0.1,
+	            max: 1
+	        },
+	        bloomRadius: {
+	            min: 8,
+	            max: 10
+	        },
+	        density: 10,
+	        growSpeed: 1000 / 60,
+	        color: {
+				rmin: 128,
+				rmax: 255,
+				gmin: 0,
+				gmax: 128,
+				bmin: 0,
+				bmax: 128,
+	            opacity: 0.1
+	        },
+	        tanAngle: 60
+	    };
+	    Garden.random = function (min, max) {
+	        return Math.random() * (max - min) + min;
+	    };
+	    Garden.randomInt = function (min, max) {
+	        return Math.floor(Math.random() * (max - min + 1)) + min;
+	    };
+	    Garden.circle = 2 * Math.PI;
+	    Garden.degrad = function (angle) {
+	        return Garden.circle / 360 * angle;
+	    };
+	    Garden.raddeg = function (angle) {
+	        return angle / Garden.circle * 360;
+	    };
+	    Garden.rgba = function (r, g, b, a) {
+	        return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+	    };
+	    Garden.randomrgba = function (rmin, rmax, gmin, gmax, bmin, bmax, a) {
+			var r = Math.round(Garden.random(rmin, rmax));
+			var g = Math.round(Garden.random(gmin, gmax));
+			var b = Math.round(Garden.random(bmin, bmax));
+			var limit = 5;
+			if (Math.abs(r - g) <= limit && Math.abs(g - b) <= limit && Math.abs(b - r) <= limit) {
+				return Garden.rgba(rmin, rmax, gmin, gmax, bmin, bmax, a);
+			} else {
+				return Garden.rgba(r, g, b, a);
+			}
+	    };
